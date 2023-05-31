3e85a168b08b18047593c5b3d9efb2c4ce68d5b9
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-05-31 13:03:43 +0300

    Update day_3_treasure_island.py

--- day_3_treasure_island.py
+++ day_3_treasure_island.py
@@ -1,3 +1,14 @@
+print('''
+888                                                          
+888                                                          
+888                                                          
+888888888d888 .d88b.  8888b. .d8888b 888  888888d888 .d88b.  
+888   888P"  d8P  Y8b    "88b88K     888  888888P"  d8P  Y8b 
+888   888    88888888.d888888"Y8888b.888  888888    88888888 
+Y88b. 888    Y8b.    888  888     X88Y88b 888888    Y8b.     
+ "Y888888     "Y8888 "Y888888 88888P' "Y88888888     "Y8888  
+''')
+
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
